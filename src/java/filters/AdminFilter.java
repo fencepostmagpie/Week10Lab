6d63818e228fc667cb05bc94eb7f91717170f1da
Aster Walton <Aster.Walton@edu.sait.ca>
2022-11-11 11:52:31 -0700

    Complete Minor bugfix

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -1,9 +1,6 @@
 package filters;
 
 import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -13,6 +10,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.Role;
+import models.User;
 
 /**
  *
@@ -29,15 +28,17 @@ public class AdminFilter implements Filter {
         //before servlet
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpSession session = httpRequest.getSession();
-        int roleID = (int)session.getAttribute("role_id");
+        User user = (User) session.getAttribute("User");
+        Role role = user.getRole();
+        int role_id = role.getRoleId();
         
-        if (roleID != 1) {
+        if (role_id != 1) {
             HttpServletResponse httpResponse = (HttpServletResponse) response;
             httpResponse.sendRedirect("notes");
             return;
         }
         
-        else if (roleID == 1) {
+        else if (role_id == 1) {
             HttpServletResponse httpResponse = (HttpServletResponse) response;
             httpResponse.sendRedirect("admin");
             return;
@@ -51,12 +52,13 @@ public class AdminFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
+
 
     @Override
     public void destroy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+ 
     }
     
 }
